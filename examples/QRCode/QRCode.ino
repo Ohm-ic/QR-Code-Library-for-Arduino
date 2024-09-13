7e57776b189e48f46360ebd61f9e14b405d54efd
Hariom Agrahari <104713869+Ohm-ic@users.noreply.github.com>
2024-09-13 15:18:26 +0530

    QRCode.ino

--- examples/QRCode/QRCode.ino
+++ examples/QRCode/QRCode.ino
@@ -1,56 +1,14 @@
-/**
- *  QRCode
- *
- *  A quick example of generating a QR code.
- *
- *  This prints the QR code to the serial monitor as solid blocks. Each module
- *  is two characters wide, since the monospace font used in the serial monitor
- *  is approximately twice as tall as wide.
- *
- */
+#include <QR_code.h>
 
-#include "qrcode.h"
+QRCodeGenerator qr;
 
 void setup() {
     Serial.begin(115200);
-
-    // Start time
-    uint32_t dt = millis();
-  
-    // Create the QR code
-    QRCode qrcode;
-    uint8_t qrcodeData[qrcode_getBufferSize(3)];
-    qrcode_initText(&qrcode, qrcodeData, 3, 0, "HELLO WORLD");
-  
-    // Delta time
-    dt = millis() - dt;
-    Serial.print("QR Code Generation Time: ");
-    Serial.print(dt);
-    Serial.print("\n");
-
-    // Top quiet zone
-    Serial.print("\n\n\n\n");
-
-    for (uint8_t y = 0; y < qrcode.size; y++) {
-
-        // Left quiet zone
-        Serial.print("        ");
-
-        // Each horizontal module
-        for (uint8_t x = 0; x < qrcode.size; x++) {
-
-            // Print each module (UTF-8 \u2588 is a solid block)
-            Serial.print(qrcode_getModule(&qrcode, x, y) ? "\u2588\u2588": "  ");
-
-        }
-
-        Serial.print("\n");
-    }
-
-    // Bottom quiet zone
-    Serial.print("\n\n\n\n");
+    delay(1000);  // Small delay for serial connection to establish
+    qr.initText("Arduino", 4, ECC_LOW);  // Increased version for larger QR cod
+    qr.printToSerial();
 }
 
 void loop() {
-
+    // No loop code required for this demo
 }
